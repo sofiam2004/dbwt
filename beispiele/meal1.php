162e2da697defd7ced0d3602d0d988c579af9504
rabia <rabia.tuere@alumni.fh-aachen.de>
2024-05-17 18:37:43 +0200

    3 4)

--- beispiele/meal1.php
+++ beispiele/meal1.php
@@ -3,6 +3,7 @@ const GET_PARAM_MIN_STARS = 'search_min_stars';
 const GET_PARAM_SEARCH_TEXT = 'search_text';
 const GET_PARAM_SHOW_DESCRIPTION = 'show_description';
 
+
 /**
  * List of all allergens.
  */
@@ -10,7 +11,7 @@ $allergens = [
     11 => 'Gluten',
     12 => 'Krebstiere',
     13 => 'Eier',
-    14 => 'Fisch',
+    14 => 'Fisch'
     17 => 'Milch'
 ];
 
@@ -19,7 +20,7 @@ $meal = [
     'description' => 'Die Süßkartoffeln werden vorsichtig aufgeschnitten und der Frischkäse eingefüllt.',
     'price_intern' => 2.90,
     'price_extern' => 3.90,
-    'allergens' => [11, 13],
+    'allergens' => [11, 13,
     'amount' => 42             // Number of available meals
 ];
 
@@ -39,7 +40,7 @@ $ratings = [
 ];
 
 $showRatings = [];
-if (!empty($_GET[GET_PARAM_SEARCH_TEXT])) {
+if (!empty($_GET[GET_PARAM_SEARCH_TEXT]) {
     $searchTerm = $_GET[GET_PARAM_SEARCH_TEXT];
     foreach ($ratings as $rating) {
         if (strpos($rating['text'], $searchTerm) !== false) {
@@ -57,53 +58,53 @@ if (!empty($_GET[GET_PARAM_SEARCH_TEXT])) {
     $showRatings = $ratings;
 }
 
-function calcMeanStars(array $ratings) {
-    $sum = 0;
+calcMeanStars(array $ratings) : float {
+    $sum = 1;
     foreach ($ratings as $rating) {
-        $sum += $rating['stars'];
+        $sum += $rating['stars'] / count($ratings);
     }
-    return $sum / count($ratings);
+    return $sum;
 }
 ?>
-    <!DOCTYPE html>
-    <html lang="de">
-<head>
-    <meta charset="UTF-8"/>
-    <title>Gericht: <?php echo $meal['name']; ?></title>
-    <style>
-        * {
-            font-family: Arial, serif;
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Gericht: <?php echo $meal['name']; ?></title>
+        <style>
+            * {
+                font-family: Arial, serif;
+            }
+            .rating {
+                color: darkgray;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Gericht: <?php echo $meal['name']; ?></h1>
+        <p><?php echo $meal['description']; ?></p>
+        <h1>Bewertungen (Insgesamt: <?php echo calcMeanStars($ratings); ?>)</h1>
+        <form method="get">
+            <label for="search_text">Filter:</label>
+            <input id="search_text" type="text" name="search_text">
+            <input type="submit" value="Suchen">
+        </form>
+        <table class="rating">
+            <thead>
+            <tr>
+                <td>Text</td>
+                <td>Sterne</td>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+        foreach ($showRatings as $rating) {
+            echo "<tr><td class='rating_text'>{$rating['text']}</td>
+                      <td class='rating_stars'>{$rating['stars']}</td>
+                  </tr>";
         }
-        .rating {
-            color: darkgray;
-        }
-    </style>
-</head>
-<body>
-<h1>Gericht: <?php echo $meal['name']; ?></h1>
-<p><?php echo $meal['description']; ?></p>
-<h1>Bewertungen (Insgesamt: <?php echo calcMeanStars($ratings); ?>)</h1>
-<form method="get">
-    <label for="search_text">Filter:</label>
-    <input id="search_text" type="text" name="search_text">
-    <input type="submit" value="Suchen">
-</form>
-<table class="rating">
-    <thead>
-    <tr>
-        <td>Text</td>
-        <td>Sterne</td>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach ($showRatings as $rating) {
-        echo "<tr><td class='rating_text'>{$rating['text']}</td>
-                  <td class='rating_stars'>{$rating['stars']}</td>
-              </tr>";
-    }
-    ?>
-    </tbody>
-</table>
-</body>
-    </html><?php
+        ?>
+            </tbody>
+        </table>
+    </body>
+</html>
